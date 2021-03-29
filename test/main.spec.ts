f42f848c679d5c632339a2e411273128100a4071
redpeacock78 <scarletpeacock78@gmail.com>
2021-03-29 20:21:12 +0900

    Change content: test/main.spec.ts

--- test/main.spec.ts
+++ test/main.spec.ts
@@ -7,26 +7,26 @@ describe('Test main function', (): void => {
         expect(await lab.dE76(color_1, color_2).then((result): string => {
             return result;
         })).toBe('10.0000');
-    })
+    });
     test('Test lab.dE94.textile()', async (): Promise<void> => {
         const color_1 = { L: 50, a: 50, b: 0 };
         const color_2 = { L: 40, a: 50, b: 0 };
         expect(await lab.dE94.textile(color_1, color_2).then((result): string => {
             return result;
         })).toBe('5.0000');
-    })
+    });
     test('Test lab.dE94.graphicArts()', async (): Promise<void> => {
         const color_1 = { L: 50, a: 50, b: 0 };
         const color_2 = { L: 40, a: 50, b: 0 };
         expect(await lab.dE94.graphicArts(color_1, color_2).then((result): string => {
             return result;
         })).toBe('10.0000');
-    })
+    });
     test('Test lab.dE00()', async (): Promise<void> => {
         const color_1 = { L: 50, a: 50, b: 0 };
         const color_2 = { L: 40, a: 50, b: 0 };
         expect(await lab.dE00(color_1, color_2).then((result): string => {
             return result;
         })).toBe('9.4706');
-    })
-})
+    });
+});
