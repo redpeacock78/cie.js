0657f5d1ca67ac4f138493b8cc3d11737ea0e1ae
redpeacock78 <scarletpeacock78@gmail.com>
2021-04-17 19:13:46 +0900

    Change content: release.sh

--- release.sh
+++ release.sh
@@ -2,20 +2,19 @@
 
 # check this version is enable to release or not
 yarn run can-npm-publish
-if [[ "${?}" == 1 ]]; then
-    exit 255
-fi
+[[ "${?}" == 1 ]] && exit 255
 
 # get current version from package.json
 TAG="$(grep version <package.json | cut -d ' ' -f 4 | tr -d ',' | tr -d \")"
-echo "add new tag to GitHub: ${TAG}"
+declare TAG
+echo "Add new tag to GitHub: ${TAG}"
 
 # Add tag to GitHub
-API_URL="https://api.github.com/repos/${REPO}/git/refs"
-
-curl -s -X POST "${API_URL}" \
-    -H "Authorization: token ${GITHUB_TOKEN}" \
-    -d @- <<EOS
+declare API_URL="https://api.github.com/repos/${REPO}/git/refs"
+curl -s \
+  -X POST "${API_URL}" \
+  -H "Authorization: token ${GITHUB_TOKEN}" \
+  -d @- <<EOS
 {
   "ref": "refs/tags/${TAG}",
   "sha": "${COMMIT}"
