17260013882cd377936bfe05f7ef343f2bfff6b7
redpeacock78 <scarletpeacock78@gmail.com>
2021-04-17 15:22:06 +0900

    Change content: src/cli/cli-dE76.ts

--- src/cli/cli-dE76.ts
+++ src/cli/cli-dE76.ts
@@ -1,60 +1,58 @@
 import * as lab from "../main";
 import { Command } from "commander";
 import { color } from "../../@types/main";
+import * as ab_end from "./utils/abnormal_end";
+import * as serialize from "./utils/serialize";
 
 const command = new Command();
 command
   .name("cie-js dE76")
-  .usage("<colors...>")
+  .usage("[colors...]")
   .description("Use the CIE 1976 color difference formula.")
   .parse(process.argv);
 
-const args: string[] = process.argv.slice(2);
 (async (): Promise<void> => {
-  if (args.length === 6) {
-    try {
-      const color_1: color = {
-        L: Number(args[0]),
-        a: Number(args[1]),
-        b: Number(args[2]),
-      };
-      const color_2: color = {
-        L: Number(args[3]),
-        a: Number(args[4]),
-        b: Number(args[5]),
-      };
-      await lab
-        .dE76(color_1, color_2)
-        .then((result: string): void => {
-          console.log(result);
-        })
-        .catch((error: unknown): void => {
-          const message: string = error
-            .toString()
-            .replace(/^Error: /, "")
-            .replace(/^ .*\n/, "")
-            .replace(/lab/g, "cie-js");
-          process.on("exit", (): void => {
-            console.error(message);
-            process.exit(1);
-          });
-        });
-    } catch {
-      process.on("exit", (): void => {
-        console.error("cie-js: Sorry. An error has occurred :_(");
-        console.error(
-          "Refer 'cie-js -h' or 'cie-js --help' for how to use the command."
-        );
-        process.exit(1);
-      });
-    }
+  let lines: string[][];
+  if (process.argv.slice(2).length) {
+    lines = [process.argv.slice(2)];
+  } else if (!process.stdin.isTTY) {
+    const buffers: Uint8Array[] = [];
+    for await (const chunk of process.stdin) buffers.push(chunk);
+    lines = Buffer.concat(buffers)
+      .toString()
+      .split(/\r?\n/)
+      .filter(Boolean)
+      .map((i: string): string[] => i.split(/,|\s|\t/));
   } else {
-    process.on("exit", (): void => {
-      console.error("cie-js: Sorry. An error has occurred :_(");
-      console.error(
-        "Refer 'cie-js -h' or 'cie-js --help' for how to use the command."
-      );
-      process.exit(1);
-    });
+    ab_end();
   }
+  await serialize(
+    lines,
+    async (args: string[]): Promise<void> => {
+      if (args.length === 6) {
+        const color_1: color = {
+          L: (args[0] as unknown) as number,
+          a: (args[1] as unknown) as number,
+          b: (args[2] as unknown) as number,
+        };
+        const color_2: color = {
+          L: (args[3] as unknown) as number,
+          a: (args[4] as unknown) as number,
+          b: (args[5] as unknown) as number,
+        };
+        await lab
+          .dE76(color_1, color_2)
+          .then((result: string): void => {
+            console.log(result);
+          })
+          .catch((): void => {
+            ab_end();
+          });
+      } else {
+        ab_end();
+      }
+    }
+  ).catch((): void => {
+    ab_end();
+  });
 })();
