de56fb1f2b20eb85edc3c9530a71dab6b036172e
redpeacock78 <scarletpeacock78@gmail.com>
2021-04-27 21:33:57 +0900

    Change content: src/cli/utils/abnormal_end.ts

--- src/cli/utils/abnormal_end.ts
+++ src/cli/utils/abnormal_end.ts
@@ -1,12 +1,17 @@
 interface ab_end {
-    (): void
+    (err_object?: Object): void
 }
 
-const ab_end: ab_end = (): void => {
+const ab_end: ab_end = (err_object?: Object): void => {
     process.on("exit", (): void => {
-        console.error("cie-js: Sorry. An error has occurred :_(");
-        console.error("Refer 'cie-js -h' or 'cie-js --help' for how to use the command.")
-        process.exit(1);
+        if (err_object.toString().indexOf('not found') != -1) {
+            console.error(err_object.toString().replace('Error: ', ''));
+            process.exit(1);
+        } else {
+            console.error("cie-js: Sorry. An error has occurred :_(");
+            console.error("Refer 'cie-js -h' or 'cie-js --help' for how to use the command.")
+            process.exit(1);
+        }
     });
 };
 
